63cb914cc368e188ec1b521d18c583ac0d7baceb
Manish <manishvishwakarma324@gmail.com>
2026-01-03 20:45:09 +0530

    feat: init next.js app with clerk auth and convex backend

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+import { AuthConfig } from "convex/server";
+
+export default {
+  providers: [
+    {
+      domain: "https://glad-elephant-80.clerk.accounts.dev/", 
+      applicationID: "convex",
+    },
+  ],
+} satisfies AuthConfig;
---
+++ convex/schema.ts
@@ -0,0 +1,47 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    userId: v.string(), // clerkId
+    email: v.string(),
+    name: v.string(),
+    isPro: v.boolean(),
+    proSince: v.optional(v.number()),
+    lemonSqueezyCustomerId: v.optional(v.string()),
+    lemonSqueezyOrderId: v.optional(v.string()),
+    lemonSqueezySubscriptionId: v.optional(v.string()),
+  }).index("by_user_id", ["userId"]),
+
+  codeExecutions: defineTable({
+    userId: v.id("users"),
+    language: v.string(),
+    code: v.string(),
+    error: v.optional(v.string()),
+    output: v.optional(v.string()),
+    result: v.optional(v.string()),
+  }).index("by_user_id", ["userId"]),
+
+  snippets: defineTable({
+    userId: v.id("users"),
+    title: v.string(),
+    language: v.string(),
+    code: v.string(),
+    userName: v.string(),
+  }).index("by_user_id", ["userId"]),
+
+  snippetComments: defineTable({
+    snippetId: v.id("snippets"),
+    content: v.string(),
+    userName: v.string(),
+    userId: v.id("users"),
+  }).index("by_snippet_id", ["snippetId"]),
+
+  stars: defineTable({
+    userId: v.id("users"),
+    snippetId: v.id("snippets"),
+  })
+    .index("by_snippet_id_and_user_id", ["snippetId"])
+    .index("by_user_id", ["userId"])
+    .index("by_snippet_id", ["snippetId"]),
+});
